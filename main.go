d5e3bbd2f63bc56443082e2b2ff7e71710fa4b7b
Darren Shepherd <darren@rancher.com>
2021-03-22 09:54:26 -0700

    Enable all auth providers

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"github.com/ibuildthecloud/finalizers/pkg/app"
 	cli "github.com/rancher/wrangler-cli"
+
+	// disable all client-go auth provides
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
 func main() {
